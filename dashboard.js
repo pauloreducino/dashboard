b625dee1c69cda6eb2230ab574f9e2bc9c411dbc
pauloreducino <paulo.a.reducino@gmail.com>
2022-08-29 12:12:55 -0300

    css 3 e javascript

---
+++ dashboard.js
@@ -0,0 +1,21 @@
+const shrink_btn = document.querySelector(".shrink-btn");
+const search = document.querySelector(".search");
+const sidebar_links = document.querySelectorAll(".sidebar-links a");
+const active_tab = document.querySelector(".active-tab");
+const shortcuts = document.querySelector(".sidebar-links h4");
+const tooltip_elements = document.querySelectorAll(".tooltip-element");
+
+let activeIndex;
+
+shrink_btn.addEventListener("click", () => {
+  document.body.classList.toggle("shrink");
+  setTimeout(moveActiveTab, 400);
+
+  shrink_btn.classList.add("hovered");
+
+  setTimeout(() => {
+    shrink_btn.classList.remove("hovered");
+  }, 500);
+});
+
+
