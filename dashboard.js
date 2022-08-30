0848a812230089d28a7a162c9e2f7292593cd65e
pauloreducino <paulo.a.reducino@gmail.com>
2022-08-30 10:16:14 -0300

    html, css e javascript edit1

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,7 @@
 const shrink_btn = document.querySelector(".shrink-btn");
 const search = document.querySelector(".search");
 const sidebar_links = document.querySelectorAll(".sidebar-links a");
-const active_tab = document.querySelector(".active-tab");
+const active_tab = document.querySelector(".active-tab");  
 const shortcuts = document.querySelector(".sidebar-links h4");
 const tooltip_elements = document.querySelectorAll(".tooltip-element");
 
@@ -18,4 +18,44 @@ shrink_btn.addEventListener("click", () => {
   }, 500);
 });
 
+search.addEventListener("click", () => {
+  document.body.classList.remove("shrink");
+  search.lastElementChild.focus();
+});
 
+function moveActiveTab() {
+  let topPosition = activeIndex * 58 + 2.5;
+
+  if (activeIndex > 3) {
+    topPosition += shortcuts.clientHeight;
+  }
+
+  active_tab.style.top = `${topPosition}px`;        
+  
+}
+
+function changeLink() {
+  sidebar_links.forEach((sideLink) => sideLink.classList.remove("active"));
+  this.classList.add("active");
+
+  activeIndex = this.dataset.active;
+
+  moveActiveTab();
+}
+
+sidebar_links.forEach((link) => link.addEventListener("click", changeLink));
+
+function showTooltip() {
+  let tooltip = this.parentNode.lastElementChild;
+  let spans = tooltip.children;
+  let tooltipIndex = this.dataset.tooltip;
+
+  Array.from(spans).forEach((sp) => sp.classList.remove("show"));
+  spans[tooltipIndex].classList.add("show");
+
+  tooltip.style.top = `${(100 / (spans.length * 2)) * (tooltipIndex * 2 + 1)}%`;
+}
+
+tooltip_elements.forEach((elem) => {
+  elem.addEventListener("mouseover", showTooltip);
+});
